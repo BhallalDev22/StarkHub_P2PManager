ff2f87a4af2b2c8d42747c99a83e86949455080a
BhallalDev22 <sukirtisingh321@gmail.com>
2020-06-23 02:07:10 +0530

    added demo new client run method and fixed some errors

--- src/P2PManager/NewClient.java
+++ src/P2PManager/NewClient.java
@@ -1,9 +1,14 @@
 package P2PManager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class NewClient extends Thread{
     private Socket socket;
+    DatabaseHandler handler = DatabaseHandler.getInstance();
 
     public NewClient(Socket socket) {
         this.socket = socket;
@@ -11,6 +16,31 @@ public class NewClient extends Thread{
 
     @Override
     public void run() {
-        
+        try {
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while(true) {
+                String sip = input.readLine();
+                System.out.println("Received client input: " + sip);
+                if(sip.equals("exit")) {
+                    break;
+                }
+                output.println(sip);
+            }
+
+        } catch(IOException e) {
+            System.out.println("Socket IO exception!\n");
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch(IOException e) {
+                System.out.println("Unable to close client socket!\n");
+                e.printStackTrace();
+            }
+        }
+
     }
 }
